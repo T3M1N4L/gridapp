7318fe57ffbeffe84ef1ac3ac86683ab67333fea
T3RM1N4L <108430379+T3M1N4L@users.noreply.github.com>
2024-12-04 23:45:24 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# gridapp
+# Grid Notes
+Grid Notes is a grid-based pinboard for note taking.
+
+Simply click and drag anywhere to create a memo and snap it to the grid. All memos can be moved, resized, deleted and are saved in your browser's local storage between sessions.
+
+To toggle between dark and light modes you can press Alt + T, catpuccin too.
+
+# Features
+* Grid-based pinboard
+* Saves notes to localstorage, you'll never miss your notes
+* Light/Dark mode - catpuccin - aesthetically pleasing
+
+ pls vote for this!! this is a a project that took me very long!
---
+++ src/js/globals.js
@@ -0,0 +1,18 @@
+export const MARGIN = 48;
+export const GRID_SIZE = 10;
+
+export const DRAG_INDEX = "99999";
+export const STATIC_INDEX = "99998";
+
+export const DEFAULT_MEMO = {
+  id: "9346dc6c-f982-48ce-9e21-f6a334b367b3",
+  text: "grid-notes is a grid-based pinboard for note taking.\n\nSimply click and drag anywhere to create a memo and snap it to the grid. All memos can be moved, resized, deleted and are saved in your browser's local storage between sessions.\n\nTo toggle between dark and light modes you can press Alt + T.\n\nTo change the current workspace you can press Shift + Alt + [0-9].",
+  position: {
+    top: 50,
+    left: 50
+  },
+  size: {
+    width: 379,
+    height: 320
+  }
+};
